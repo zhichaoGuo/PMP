9935004600300177cdbc8b7d979168f6ca4d263f
yaki.guo <yaki.guo@htek.com>
2023-01-12 18:50:24 +0800

    init database

--- app/models.py
+++ app/models.py
@@ -1,31 +1,54 @@
-from flask_login import UserMixin
+import datetime
+
 from werkzeug.security import generate_password_hash
 
-from app import db, login_manager
+from app import db
 
 
-class Users(db.Model, UserMixin):
-    __tablename__ = 'Users'
+class Model(db.Model):  # 型号表
+    __tablename__ = 'Model'
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True)
-    email = db.Column(db.String(64), unique=True)
-    password = db.Column(db.LargeBinary)
-
-    oauth_github = db.Column(db.String(100), nullable=True)
-
-    def __init__(self, **kwargs):
-        for property, value in kwargs.items():
-
-            if hasattr(value, '__iter__') and not isinstance(value, str):
-                # the ,= unpack of a singleton fails PEP8 (travis flake8 test)
-                value = value[0]
-
-            if property == 'password':
-                value = generate_password_hash(value)  # we need bytes here (not plain str)
-                # need = check_password_hash(password,value)
-            setattr(self, property, value)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64), unique=True, nullable=False)
+    status = db.Column(db.Boolean(), default=False)
 
     def __repr__(self):
-        return str(self.username)
+        return str(self.name)
 
+
+class Way(db.Model):  # 激励策略表
+    __tablename__ = 'Way'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    model_id = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(64), unique=True)
+    start_time = db.Column(db.DateTime(), default=datetime.datetime.now())
+    status = db.Column(db.Boolean(), default=False)
+
+
+class Node(db.Model):  # 激励节点表
+    __tablename__ = 'Node'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    way_id = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    percentage = db.Column(db.String(64))
+
+
+class Record(db.Model):  # 销售记录表
+    __tablename__ = 'Record'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64), nullable=False)
+    sale_time = db.Column(db.DateTime(), default=datetime.datetime.now())
+
+
+class Detail(db.Model):  # 销售明细表
+    __tablename__ = 'Detail'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    record_id = db.Column(db.Integer, nullable=False)
+    model_id = db.Column(db.Integer, nullable=False)
+    sale_price = db.Column(db.Float, nullable=False)
+    sale_time = db.Column(db.DateTime(), default=datetime.datetime.now())
+    sale_number = db.Column(db.Integer, nullable=False)
